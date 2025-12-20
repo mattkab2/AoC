5d3bcf8e977a2546e0cf9d3d53eb11db3c1bf6d6
Matt Kabelitz <mattkab2@gmail.com>
2025-12-20 14:42:36 -0500

    day 7 complete

---
+++ 2025/cpp/07/part2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+    ifstream input;
+    input.open(argv[1], ios::in);
+    string line;
+
+    std::vector<std::vector<int>> geometry;
+    std::vector<std::vector<long>> state;
+    while (getline(input, line)) {
+        std::vector<int> tmp;
+        for (const char &c : line) {
+            switch(c) {
+            case '.':
+                tmp.push_back(0);
+                break;
+            case 'S':
+                tmp.push_back(-1);
+                break;
+            case '^':
+                tmp.push_back(1);
+                break;
+            }
+        }
+        geometry.push_back(tmp);
+        state.emplace_back(tmp.size(), 0);
+    }
+
+    long numSplits = 0;
+    for (size_t row = 0; row < geometry.size(); row++) {
+        for (size_t col = 0; col < geometry.front().size(); col++) {
+            if (geometry[row][col] == -1) {
+                state[row][col] = 1;
+            }
+            if (row > 0 && state[row-1][col] > 0) {
+                if (geometry[row][col] == 0) {
+                    state[row][col] += state[row-1][col];
+                } else if (geometry[row][col] == 1 && state[row-1][col] > 0) {
+                    numSplits++;
+                    if (geometry[row][col-1] == 0) {state[row][col-1] += state[row-1][col];}
+                    if (geometry[row][col+1] == 0) {state[row][col+1] += state[row-1][col];}
+                }
+            }
+        }
+    }
+
+    long ret = 0;
+    for (long ct : state.back()) {
+        ret += ct;
+    }
+
+    input.close();
+    cout << "#####################\n";
+    cout << ret << '\n';
+    cout << "#####################\n";
+    return 0;
+}
