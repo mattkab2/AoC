97683e28176ff4172fc3f136591841703e5324b2
Matt Kabelitz <mattkab2@gmail.com>
2025-12-22 10:15:34 -0500

    Finish day 3

--- 2025/cpp/03/part2.cpp
+++ 2025/cpp/03/part2.cpp
@@ -1,25 +1,29 @@
+#include <array>
 #include <iostream>
 #include <fstream>
-#include <sstream>
+#include <limits>
+#include <map>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-ulong callCount = 0;
+// Because c++ is stupid
+std::array<long, 12> pow10 = {long(1e11), long(1e10), long(1e9), long(1e8), long(1e7), long(1e6), long(1e5), long(1e4), long(1e3), long(1e2), 10, 1};
 
-long max_recursive(const std::vector<int> &data, int start, long total, int numDigits) {
-    callCount++;
+long max_recursive(const std::vector<int> &data, int start, int numDigits, std::map<std::pair<int, int>, long> &cache) {
     if (numDigits == 12) {
-        return total;
-    }
-    if (start == data.size()) {
         return 0;
     }
-    return std::max(
-        max_recursive(data, start+1, 10*total + data[start], numDigits+1),
-        max_recursive(data, start+1,    total,               numDigits)
+    if (start == data.size()) {
+        return std::numeric_limits<long>::min();
+    }
+    long result = std::max(
+        pow10.at(numDigits)*data[start] + (cache.contains({start+1, numDigits+1}) ? cache.at({start+1, numDigits+1}) : max_recursive(data, start+1, numDigits+1, cache)),
+        (cache.contains({start+1, numDigits}) ? cache.at({start+1, numDigits}) : max_recursive(data, start+1, numDigits, cache))
     );
+    cache[{start, numDigits}] = result;
+    return result;
 }
 
 int main(int argc, char *argv[]) {
@@ -37,9 +41,9 @@ int main(int argc, char *argv[]) {
 
     long ret = 0;
     for (const auto &bat : jolts) {
-        long max = max_recursive(bat, 0, 0, 0);
+        std::map<std::pair<int, int>, long> cache;
+        long max = max_recursive(bat, 0, 0, cache);
         std::cout << max << std::endl;
-        std::cout << callCount << std::endl;
         ret += max;
     }
 
